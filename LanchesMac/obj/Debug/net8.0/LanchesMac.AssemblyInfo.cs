1fb9a734147a4db1dd1c7c3b7c8a8979acd55984
Danilo <daniloarrabal1@hotmail.com>
2024-02-08 10:05:11 -0300

    Add PartialCarousel e Images

--- LanchesMac/obj/Debug/net8.0/LanchesMac.AssemblyInfo.cs
+++ LanchesMac/obj/Debug/net8.0/LanchesMac.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82828a97bf937580b603b4e7c7affc86b6800b65")]
 [assembly: System.Reflection.AssemblyProductAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
