ecd937a05b97f53199183e4de891278b12f88c3d
Danilo <daniloarrabal1@hotmail.com>
2024-02-10 08:33:34 -0300

    Migration

--- LanchesMac/obj/Debug/net8.0/LanchesMac.AssemblyInfo.cs
+++ LanchesMac/obj/Debug/net8.0/LanchesMac.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9289955e7e247217d36d391c827e6259229ef8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
