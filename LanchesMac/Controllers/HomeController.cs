2ca9ecbbb2c0f92cd3c35dfd91b89754f0989918
Danilo <daniloarrabal1@hotmail.com>
2024-02-14 10:04:01 -0300

    Ajustes Lanche View

--- LanchesMac/Controllers/HomeController.cs
+++ LanchesMac/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace LanchesMac.Controllers
     {
         public IActionResult Index()
         {
+            TempData["Nome"] = "Danilo";
             return View();
         }
         public IActionResult Demo()
