dc4c6296323a32245657fd9054e81d33acdd9220
Danilo <daniloarrabal1@hotmail.com>
2024-02-14 09:44:45 -0300

    Lanche Controller e View

--- LanchesMac/obj/Debug/net8.0/LanchesMac.AssemblyInfo.cs
+++ LanchesMac/obj/Debug/net8.0/LanchesMac.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a2f4bfee0e698edea9a561b017b3a987be4942")]
 [assembly: System.Reflection.AssemblyProductAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
