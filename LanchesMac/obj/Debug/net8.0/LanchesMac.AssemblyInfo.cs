fbc6d0ee6d4e9f0a8aaa47b69f17a91a0c2bfcb5
Danilo <daniloarrabal1@hotmail.com>
2024-03-12 17:57:46 -0300

    Login Controller e VM

--- LanchesMac/obj/Debug/net8.0/LanchesMac.AssemblyInfo.cs
+++ LanchesMac/obj/Debug/net8.0/LanchesMac.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7de66af73ed2ed3dd975c75808947d00c7f4e77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
