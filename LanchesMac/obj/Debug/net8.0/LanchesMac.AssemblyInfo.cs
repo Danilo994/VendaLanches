22485af27c806778048965a190773e535eee9f6c
Danilo <daniloarrabal1@hotmail.com>
2024-04-20 08:26:06 -0300

    rodape e logo

--- LanchesMac/obj/Debug/net8.0/LanchesMac.AssemblyInfo.cs
+++ LanchesMac/obj/Debug/net8.0/LanchesMac.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f62b397af59a6f13b5c825ef9a3746920509ad3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f157b021eb2f0c26c7e76ec3cb5893ea7e34d21b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
